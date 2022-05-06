63e6f4cb1f5111d322cbe4df17e0440e70813adc
lucas-montenegro <lucasmaassuncao@gmail.com>
2022-05-06 17:01:25 -0300

    Completing WidgetForm component

--- src/Components/WidgetForm/index.tsx
+++ src/Components/WidgetForm/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton";
+
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -29,36 +32,36 @@ const feedbackTypes = {
     }
 }
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-    const [feedbackType, setFeedback] = useState<FeedbackType | null>(null)
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false)
+
+    function handleRestartFeedback() {
+        setFeedbackSent(false)
+        setFeedbackType(null)
+    }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading=6">
-                    Deixe seu feedback
-                </span>
+            { feedbackSent 
+              ? <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+              : ( 
+                <>
+                    { !feedbackType 
+                      ? <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                      : <FeedbackContentStep 
+                          feedbackType={feedbackType} 
+                          onFeedbackRestartRequested={handleRestartFeedback} 
+                          onFeedbackSent={() => setFeedbackSent(true)}
+                      />
+                    }
+                </>
+              )
+            }
 
-                <CloseButton />
-            </header>
-
-            <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key, value]) => {
-                    return (
-                        <button 
-                          key={key}
-                          className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                          onClick={() => setFeedback(key as FeedbackType)}
-                          type="button"
-                        > 
-                            <img src={value.image.source} alt={value.image.alt} />
-                            <span>{value.title}</span>
-                        </button>
-                    )
-                }) }
-            </div>
+            
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
